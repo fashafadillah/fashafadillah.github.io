eda36af49f93120cf86660fdedf1232b8af721e8
fasfashann <saintgloria.jkt@gmail.com>
2022-05-15 20:12:23 +0700

    mengubah navigasi bar

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+const btnTabs = document.querySelector(".btn-tabs");
+const btn = document.querySelector(".menu-section ul li");
+
+btnTabs.addEventListener("click", function () {
+  btn.classList.toggle("active");
+});
