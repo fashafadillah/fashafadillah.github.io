d1419582db37215a2964d77205c5a683bb41a06d
fasfashann <saintgloria.jkt@gmail.com>
2022-05-24 00:37:18 +0700

    mengatur state active pada navigasi bar

--- activePage.js
+++ activePage.js
@@ -1,10 +1,10 @@
-document.querySelectorAll(".nav-link").forEach((link) => {
-  if (link.href === window.location.href) {
+document.querySelectorAll(".home").forEach((link) => {
+  if (link.hostname === window.location.hostname) {
     link.classList.add("active");
   }
 });
-document.querySelectorAll(".aria-current").forEach((link) => {
-  if (link.hostname === window.location.hostname) {
+document.querySelectorAll(".nav-link").forEach((link) => {
+  if (link.href === window.location.href) {
     link.classList.add("active");
   }
 });
